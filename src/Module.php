95ea60ab1f3d702b55c9d1127647413e4b065b11
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-03-21 09:03:55 +0200

    Added src() method Added compression support Added controller for serving static resource Added automatic rewriting for static resources via controller

--- src/Module.php
+++ src/Module.php
@@ -8,13 +8,26 @@ namespace samsonphp\view;
 use samsonframework\core\ResourcesInterface;
 use samsonframework\core\SystemInterface;
 use samsonframework\view\Generator;
+use samsonphp\Event\Event;
 
 /**
  * SamsonPHP view module
  * @package samsonphp\view
  */
-class Module extends \samson\core\ExternalModule
+class Module extends \samson\core\ExternalModule implements \samsonframework\core\CompressInterface
 {
+    /** View handling events */
+    const EVENT_VIEW_HANDLER = 'samsonphp.view.handler';
+    const EVENT_VIEW_COMPRESSION = 'samsonphp.view.compression';
+
+    /** Pattern for compressing $this->src() calls with resource path */
+    const SRC_COMPRESSION_PATTERN = '/(<\?=|<\?php\s*echo\s*\(?)\s*\$this->src\(\s*(\'|\")\s*(src|www)\/(?<path>[^\'\"]+)(\'|\")\s*\)\s*\?>/';
+    /** Pattern for replacing $this->src() calls with controller url */
+    const SRC_PATTERN = '/(<\?=|<\?php\s*echo\s*\(?)\s*\$this->src\(\s*(\'|\")(?<path>[^\'\"]+)(\'|\")\s*\)\s*\?>/';
+
+    /** @var string Module identifier */
+    protected $id = STATIC_RESOURCE_HANDLER;
+
     /** @var Generator */
     protected $generator;
 
@@ -41,6 +54,85 @@ class Module extends \samson\core\ExternalModule
 
         // Register View class file autoloader
         spl_autoload_register(array($this, 'autoload'));
+    }
+
+    /**
+     * This method should be used to override generic compression logic.
+     *
+     * @param mixed $obj Pointer to compressor instance
+     * @param array|null $code Collection of already compressed code
+     *
+     * @return bool False if generic compression needs to be avoided
+     */
+    public function beforeCompress(&$obj = null, array &$code = null)
+    {
+
+    }
+
+    /**
+     * This method is called after generic compression logic has finished.
+     *
+     * @param mixed      $obj  Pointer to compressor instance
+     * @param array|null $code Collection of already compressed code
+     *
+     * @return bool False if generic compression needs to be avoided
+     */
+    public function afterCompress(&$obj = null, array &$code = null)
+    {
+        $this->generator->generate($this->cache_path, array($this, 'compressionHandler'));
+        // Iterate through generated php code
+        foreach ($this->generator->metadata as $file => $metadata) {
+            // Compress generated php code
+            $obj->compress_php($metadata->generatedPath, $this, $code, $metadata->namespace);
+        }
+    }
+
+    /**
+     * Generator view code handler.
+     *
+     * @param string $viewCode Source view code
+     *
+     * @return string Modified view code
+     */
+    public function viewHandler($viewCode)
+    {
+        // Fire event
+        Event::fire(self::EVENT_VIEW_HANDLER, array(&$viewCode));
+
+        // Find all paths to intermediate controller
+        if (preg_match_all(self::SRC_PATTERN, $viewCode, $matches)) {
+            for ($i = 0, $size = count($matches['path']); $i < $size; $i++) {
+                // Remove function call just leave path related to src(for modules) or www(for local)
+                $viewCode = str_replace($matches[0][$i], '/' . STATIC_RESOURCE_HANDLER . '/?p=' . $matches['path'][$i], $viewCode);
+            }
+        }
+
+        // Return modified view code
+        return $viewCode;
+    }
+
+    /**
+     * Compression view code handler.
+     *
+     * @param string $viewCode Source view code
+     *
+     * @return string Modified view code
+     */
+    public function compressionHandler($viewCode)
+    {
+        // Fire event
+        Event::fire(self::EVENT_VIEW_COMPRESSION, array(&$viewCode));
+
+        // Find all pathes to intermediate controller
+        if (preg_match_all(self::SRC_COMPRESSION_PATTERN, $viewCode, $matches)) {
+            for ($i = 0, $size = count($matches['path']); $i < $size; $i++) {
+                // Remove function call just leave path related to src(for modules) or www(for local)
+                $viewCode = str_replace($matches[0][$i], $matches['path'][$i], $viewCode);
+            }
+        }
+
+        // Return modified view code
+        return $viewCode;
     }
 
     /**
@@ -71,7 +163,7 @@ class Module extends \samson\core\ExternalModule
         //$this->generator->scan(__SAMSON_CWD__.'/app');
         $signature = $this->generator->hash();
         if ($this->cache_refresh($signature)) {
-            $this->generator->generate($this->cache_path);
+            $this->generator->generate($this->cache_path, array($this, 'viewHandler'));
             // Store cache file
             file_put_contents($signature, '');
         }
